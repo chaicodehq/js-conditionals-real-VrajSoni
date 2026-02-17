59eaaff18ab565bbb238275914179cdeca03086a
VrajSoni <vrajsoni624@gmail.com>
2026-02-17 20:54:56 +0530

    pass: JS conditionals 10-tip

--- src/10-tip-calculator.js
+++ src/10-tip-calculator.js
@@ -30,5 +30,39 @@
  * @returns {{ tipPercentage: number, tipAmount: number, totalAmount: number } | null}
  */
 export function calculateTip(billAmount, serviceRating) {
-  // Your code here
+
+  if ( billAmount <=0 ){
+    return null;
+  }
+  let tipPercentage = 0;
+
+  if( serviceRating === 1){
+    tipPercentage = 5;
+  }
+  else if(serviceRating === 2){
+    tipPercentage = 10;
+  }
+  else if(serviceRating === 3){
+    tipPercentage = 15;
+  }
+  else if(serviceRating === 4){
+    tipPercentage = 20;
+  }
+  else if(serviceRating === 5){
+    tipPercentage = 25;
+  }
+  else{
+    return null;
+  }
+  
+  let rawTip = billAmount * (tipPercentage / 100);
+  const tipAmount = Number(rawTip.toFixed(2))
+
+  const totalAmount = Number((billAmount+tipAmount).toFixed(2))
+
+  return{
+    tipPercentage: tipPercentage,
+        tipAmount: tipAmount,
+        totalAmount: totalAmount
+  }
 }
